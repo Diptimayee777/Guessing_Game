56e37b5cc91d5f0cda4976afba45f37dc65982a4
Diptimayee777 <diptimayeepatra07@gmail.com>
2024-06-25 00:15:15 +0530

    Guessinggame.py

--- Guessinggame.py
+++ Guessinggame.py
@@ -1,13 +1,27 @@
 import random
 
-r=random.randint(1,100)
-guess=0
+# Generate a random number between 1 and 100
+secret_number = random.randint(1, 100)
 
-while guess!=r:
-    guess=int(input("Enter guess:"))
-    if (guess<r):
-        print("Guess higher!")
-    elif(guess>r):
-        print("guess lower!")
+# Initialize the number of attempts
+attempts = 0
+
+print("Welcome to the number guessing game!")
+print("I'm thinking of a number between 1 and 100.")
+
+while True:
+    # Prompt the user to input their guess
+    user_guess = int(input("Enter your guess: "))
+
+    # Increment the number of attempts
+    attempts += 1
+
+    # Compare the user's guess to the secret number
+    if user_guess < secret_number:
+        print("Guess higher! Try again.")
+    elif user_guess > secret_number:
+        print("Guess lower! Try again.")
     else:
-        print("You won!")
+        # The user guessed correctly!
+        print(f" Congratulations! You guessed the number in {attempts} attempts.")
+        break
